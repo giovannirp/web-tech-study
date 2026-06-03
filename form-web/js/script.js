cd886ae52a915e25e1a36f838f33a9e78dbe3d28
giovannirp <giovanni_ge@hotmail.com>
2026-06-03 11:15:56 -0300

    feat: finalizando o layout de css e adicionando javascript no projeto

---
+++ form-web/js/script.js
@@ -0,0 +1,16 @@
+/*
+    Criando variáveis
+*/
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+/*
+    Criando função
+*/
+button.addEventListener("click", (event) => {
+   event.preventDefault();
+   
+   // Incluir itens no HTML
+
+});
