57da0533a5dad59d94a0ce5dc92b7572845e0737
giovannirp <giovanni_ge@hotmail.com>
2026-06-08 10:45:46 -0300

    feat: adicionando codigos novos

---
+++ exemplo-js/tipo-dados.js
@@ -0,0 +1,18 @@
+// string
+let nome = "Giovanni Ribeiro";
+
+// Inteiro
+let idade = 38;
+
+// Booleano 
+let casado = false;
+
+// Números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+// Resultados
+console.log("Meu nome é: " + nome);
+console.log("Minha idade é: " + idade);
+console.log("Casado? " + casado);
+console.log("R$", valorProduto);
+console.log(typeof nome);
