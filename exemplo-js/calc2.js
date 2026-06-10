6b6866e114f3c4bd020ab909f9528e57072e96aa
giovannirp <giovanni_ge@hotmail.com>
2026-06-10 09:08:50 -0300

    feat: adicionando função para calculo e convertendo

---
+++ exemplo-js/calc2.js
@@ -0,0 +1,10 @@
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"));
+// Number()
+
+// Operações
+let soma = num1 + num2;
+
+console.log(soma);
+
+console.log(typeof soma)
