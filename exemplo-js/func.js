3edb5626f5b863b22adb74b3dd54beb1ae9badc9
giovannirp <giovanni_ge@hotmail.com>
2026-06-10 08:45:05 -0300

    feat: add

---
+++ exemplo-js/func.js
@@ -0,0 +1,19 @@
+function minhaFuncao() {
+    document.write("Minha função");
+}
+
+function pularLinha() {
+    document.write("<br><br>");
+}
+
+function mostrarNome(nome) {
+    pularLinha()
+    document.write(nome);
+    pularLinha();
+}
+
+minhaFuncao();
+pularLinha();
+minhaFuncao();
+mostrarNome("Giovanni");
+mostrarNome("Doris");
