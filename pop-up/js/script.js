944034e89706b43b63b93bb0b5a6f22a66d38b5f
giovannirp <giovanni_ge@hotmail.com>
2026-06-15 10:43:16 -0300

    feat: finalizando modal em js

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -1,1 +1,18 @@
-// alert("Pop-up");
+const button = document.querySelector("button");
+const popup = document.querySelector(".popup-wrapper");
+
+button.addEventListener("click", function(event) {
+   popup.classList.add("d-block");
+});
+
+popup.addEventListener("click", function(event) {
+    const classOfClickElement = event.target.classList[0];
+    const classNameListArray = ["popup-close", "popup-link", "popup-wrapper"];
+
+    const isClassList = classNameListArray.includes(classOfClickElement);
+
+    if (isClassList) {
+        popup.classList.remove("d-block")
+    }
+    
+});
