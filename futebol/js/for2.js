07b125130ea38fe3c8fb4e92fe37500426363319
Giovanni <giovanni_ge@hotmail.com>
2026-07-29 09:00:05 -0300

    feat: adicionando arquivos do projeto e exemplo com for

---
+++ futebol/js/for2.js
@@ -0,0 +1,5 @@
+let frutas = ["maça", "banana", "pera", "uva"];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
---
+++ futebol/js/script.js
@@ -0,0 +1,17 @@
+const jogadores = [
+    "Neymar",
+    "Vinicius Júnior",
+    "Rodrygo",
+    "Endrick",
+    "Casemiro",
+    "Alisson",
+    "Marquinhos",
+    "Gabriel Magalhães",
+    "Bruno Guimarães",
+    "Raphinha"
+];
+
+function listarJogadores() {
+    alert("Teste lista")
+}
+
