1df30092271e909343e61cda6081c835229a38c8
Giovanni <giovanni_ge@hotmail.com>
2026-08-11 16:43:34 -0300

    feat: add

--- futebol/js/script.js
+++ futebol/js/script.js
@@ -8,10 +8,16 @@ const jogadores = [
     "Marquinhos",
     "Gabriel Magalhães",
     "Bruno Guimarães",
-    "Raphinha"
+    "Raphinha",
+    "Dimba"
 ];
 
 function listarJogadores() {
-    alert("Teste lista")
+    let lista = document.getElementById("listaJogadores");
+    lista.innerHTML = "";
+
+   for (let i = 0; i < jogadores.length; i++) {
+    lista.innerHTML += "<li>" + jogadores[i] + "</li>";
+   }
 }
 
