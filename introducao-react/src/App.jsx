48fec3b747522b49dc5ab803ba618bb94e1e3add
giovannirp <giovanni_ge@hotmail.com>
2026-08-12 10:18:17 -0300

    feat: adicionando projetos em React e criando Header

---
+++ introducao-react/src/App.jsx
@@ -0,0 +1,29 @@
+import Header from "./Components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+      <section>
+        <article>
+          <h1>Lorem Ipsun</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque cumque delectus, maxime et voluptatem quae aut qui nisi placeat deleniti sapiente adipisci sunt deserunt id. Voluptatibus asperiores commodi explicabo placeat!</p>
+        </article>
+
+        <article>
+          <h1>Lorem Ipsun</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque cumque delectus, maxime et voluptatem quae aut qui nisi placeat deleniti sapiente adipisci sunt deserunt id. Voluptatibus asperiores commodi explicabo placeat!</p>
+        </article>
+        <hr />
+      </section>
+
+      <footer>
+        <p>Lorem ipsum dolor sit amet con.</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
